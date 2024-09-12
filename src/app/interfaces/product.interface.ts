2f49ce9b918e4fbb8a6248a7e30a03affa0a9da6
Derik Daniel Daza <derik3d@gmail.com>
2024-09-12 17:53:26 -0500

    rm unused data from product interface

--- src/app/interfaces/product.interface.ts
+++ src/app/interfaces/product.interface.ts
@@ -1,9 +1,6 @@
 import { Identifiable } from "../crud/interfaces/Identificable.interface";
 
   export interface Product extends Identifiable{
-    id?: string;  // Optional for create operations
-    productName: string;
-    productDescription: string;
-    fkProductTypeId: number;
+    id?: string;
   }
   
