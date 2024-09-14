ff2282d5b13c7d231f9eb7659f58ba7b29fb5828
Derik Daniel Daza <derik3d@gmail.com>
2024-09-14 11:53:56 -0500

    set up navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,42 @@ import { ProductsComponent } from './products/products.component';
 import { OrdersComponent } from './orders/orders.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductComponent } from './component/product/product.component';
+import { ProductTypeComponent } from './component/product-type/product-type.component';
+import { InventoryItemComponent } from './component/inventory-item/inventory-item.component';
+import { OrderComponent } from './component/order/order.component';
+import { StatusComponent } from './component/status/status.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     ProductComponent,
+    ProductTypeComponent,
+    InventoryItemComponent,
+    OrderComponent,
+    StatusComponent,
     ProductsComponent,
     OrdersComponent,
     HttpClientModule,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive],
+    RouterLinkActive,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Big store manager';
+
+  routes = [
+    { path: 'products' },
+    { path: 'orders' },
+    { path: 'product' },
+    { path: 'product-type' },
+    { path: 'inventory-item' },
+    { path: 'order' },
+    { path: 'status' },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {ProductsComponent} from './products/products.component';
 import {OrdersComponent} from './orders/orders.component';
 import { ProductComponent } from './component/product/product.component';
+import { ProductTypeComponent } from './component/product-type/product-type.component';
+import { InventoryItemComponent } from './component/inventory-item/inventory-item.component';
+import { StatusComponent } from './component/status/status.component';
+
 
 
 
@@ -9,5 +13,9 @@ export const routes: Routes = [
     { path: 'products', component: ProductsComponent },
     { path: 'orders', component: OrdersComponent },
     { path: 'product', component: ProductComponent },
+    { path: 'product-type', component: ProductTypeComponent },
+    { path: 'inventory-item', component: InventoryItemComponent },
+    { path: 'order', component: OrdersComponent },
+    { path: 'status', component: StatusComponent },
     { path: '', redirectTo: '/products' , pathMatch: 'full' },
   ];
