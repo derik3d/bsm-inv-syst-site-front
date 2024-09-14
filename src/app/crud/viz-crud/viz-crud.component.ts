114c52cc1c14d247bc2cab374b0f0a19d0b6fc46
Derik Daniel Daza <derik3d@gmail.com>
2024-09-14 13:51:07 -0500

    add material style

--- src/app/crud/viz-crud/viz-crud.component.ts
+++ src/app/crud/viz-crud/viz-crud.component.ts
@@ -1,16 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { ProductComponent } from '../../component/product/product.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { GeneralCrudComponent } from '../general-crud/general-crud.component';
 import { Identifiable } from '../interfaces/Identificable.interface';
+
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-viz-crud',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
   ],
   templateUrl: './viz-crud.component.html',
   styleUrl: './viz-crud.component.css'
