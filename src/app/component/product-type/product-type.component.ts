43af5d57f5b1f9085e8e44dcfe801403ceb08734
Derik Daniel Daza <derik3d@gmail.com>
2024-09-14 19:42:31 -0500

    fix product type

--- src/app/component/product-type/product-type.component.ts
+++ src/app/component/product-type/product-type.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GeneralCrudComponent } from '../../crud/general-crud/general-crud.component';
 import { GeneralCrudService } from '../../crud/services/general-crud.service';
-import { Product } from '../../interfaces/product.interface';
 import { CommonModule } from '@angular/common';
 import { VizCrudComponent } from '../../crud/viz-crud/viz-crud.component';
 import { ProductTypeService } from './product-type.service';
+import { ProductType } from '../../interfaces/product-type.interface';
 
 @Component({
   selector: 'app-product-type-crud',
@@ -19,18 +19,12 @@ import { ProductTypeService } from './product-type.service';
   ],
   providers: [{ provide: GeneralCrudService, useClass: ProductTypeService }],
 })
-export class ProductTypeComponent extends GeneralCrudComponent<Product> {
+export class ProductTypeComponent extends GeneralCrudComponent<ProductType> {
   data = {
-    id: '66e19c799672e0d3fd60d1a3',
-    additional_info: {
-      description: 'A high-end smartphone with advanced features.',
-    },
-    creation: 1726061689.367564,
-    order_info:
-      '{"_id": "66e19c799672e0d3fd60d1b1", "creation": 1726061689.3864436, "updated": 1726061689.3864436, "status": null, "status_trace": [], "client": {"name": "Client A", "email": "client1@example.com"}, "inventory_items": [{"_id": "66e19c799672e0d3fd60d1a3", "creation": 1726061689.367564, "product_id": 1, "product_name": "Smartphone", "serial_number": "SN11", "additional_info": {"description": "A high-end smartphone with advanced features."}, "order_info": null}]}',
-    product_id: 1,
-    product_name: 'Smartphone',
-    serial_number: 'SN11',
+    id: 1,
+    nemo: 'PUT',
+    parent_type_id: null,
+    type_name: 'public item',
   };
 
   override getData(): Object {
