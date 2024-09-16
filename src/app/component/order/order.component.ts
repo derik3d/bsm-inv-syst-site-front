dc0a5b80e56b624f55a5e39a17b48cf779834fee
Derik Daniel Daza <derik3d@gmail.com>
2024-09-16 09:03:39 -0500

    temp ignore array

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -17,44 +17,67 @@ import { OrderService } from './order.service';
 })
 export class OrderComponent extends GeneralCrudComponent<Order> {
   data = {
-    id: '66e19c799672e0d3fd60d1b1',
-    client: {
-      email: 'client1@example.com',
-      name: 'Client A',
+    "client": {
+      "email": "client6@example.com",
+      "name": "Client F"
     },
-    creation: 1726061689.3864436,
-    inventory_items: [
+    "creation": 1726414285.305219,
+    "id": "66e6fdcd2db33881185dc1e3",
+    "inventory_items": [
       {
-        additional_info: {
-          description: 'A high-end smartphone with advanced features.',
+        "additional_info": {
+          "description": "A personalized ID card with unique identification."
         },
-        creation: 1726061689.367564,
-        id: '66e19c799672e0d3fd60d1a3',
-        order_info: null,
-        product_id: 1,
-        product_name: 'Smartphone',
-        serial_number: 'SN11',
+        "creation": 1726414285.2872214,
+        "id": "66e6fdcd2db33881185dc1d5",
+        "order_info": null,
+        "product_id": 3,
+        "product_name": "ID Card",
+        "serial_number": "SN32"
       },
-    ],
-    status: {
-      creation: 1726061689.3997486,
-      description: 'Order has been created.',
-      id: '66e19c799672e0d3fd60d1b7',
-      order_info:
-        '{"_id": "66e19c799672e0d3fd60d1b1", "creation": 1726061689.3864436, "updated": 1726061689.3864436, "status": null, "status_trace": [], "client": {"name": "Client A", "email": "client1@example.com"}, "inventory_items": [{"_id": "66e19c799672e0d3fd60d1a3", "creation": 1726061689.367564, "product_id": 1, "product_name": "Smartphone", "serial_number": "SN11", "additional_info": {"description": "A high-end smartphone with advanced features."}, "order_info": null}]}',
-      status: 'Created',
-    },
-    status_trace: [
       {
-        creation: 1726061689.3997486,
-        description: 'Order has been created.',
-        id: '66e19c799672e0d3fd60d1b7',
-        order_info:
-          '{"_id": "66e19c799672e0d3fd60d1b1", "creation": 1726061689.3864436, "updated": 1726061689.3864436, "status": null, "status_trace": [], "client": {"name": "Client A", "email": "client1@example.com"}, "inventory_items": [{"_id": "66e19c799672e0d3fd60d1a3", "creation": 1726061689.367564, "product_id": 1, "product_name": "Smartphone", "serial_number": "SN11", "additional_info": {"description": "A high-end smartphone with advanced features."}, "order_info": null}]}',
-        status: 'Created',
-      },
+        "additional_info": {
+          "description": "A contract signed personally."
+        },
+        "creation": 1726414285.2872214,
+        "id": "66e6fdcd2db33881185dc1dc",
+        "order_info": null,
+        "product_id": 7,
+        "product_name": "Personal On-place Agreement",
+        "serial_number": "SN71"
+      }
     ],
-    updated: 1726061689.4212673,
+    "status": {
+      "creation": 1726414285.328267,
+      "description": "Order has been delivered.",
+      "id": "66e6fdcd2db33881185dc1ee",
+      "order_info": "{\"_id\": \"66e6fdcd2db33881185dc1e3\", \"creation\": 1726414285.305219, \"updated\": 1726414285.305219, \"status\": null, \"status_trace\": [], \"client\": {\"name\": \"Client F\", \"email\": \"client6@example.com\"}, \"inventory_items\": [{\"_id\": \"66e6fdcd2db33881185dc1d5\", \"creation\": 1726414285.2872214, \"product_id\": 3, \"product_name\": \"ID Card\", \"serial_number\": \"SN32\", \"additional_info\": {\"description\": \"A personalized ID card with unique identification.\"}, \"order_info\": null}, {\"_id\": \"66e6fdcd2db33881185dc1dc\", \"creation\": 1726414285.2872214, \"product_id\": 7, \"product_name\": \"Personal On-place Agreement\", \"serial_number\": \"SN71\", \"additional_info\": {\"description\": \"A contract signed personally.\"}, \"order_info\": null}]}",
+      "status": "Delivered"
+    },
+    "status_trace": [
+      {
+        "creation": 1726414285.3252213,
+        "description": "Order has been created.",
+        "id": "66e6fdcd2db33881185dc1ec",
+        "order_info": "{\"_id\": \"66e6fdcd2db33881185dc1e3\", \"creation\": 1726414285.305219, \"updated\": 1726414285.305219, \"status\": null, \"status_trace\": [], \"client\": {\"name\": \"Client F\", \"email\": \"client6@example.com\"}, \"inventory_items\": [{\"_id\": \"66e6fdcd2db33881185dc1d5\", \"creation\": 1726414285.2872214, \"product_id\": 3, \"product_name\": \"ID Card\", \"serial_number\": \"SN32\", \"additional_info\": {\"description\": \"A personalized ID card with unique identification.\"}, \"order_info\": null}, {\"_id\": \"66e6fdcd2db33881185dc1dc\", \"creation\": 1726414285.2872214, \"product_id\": 7, \"product_name\": \"Personal On-place Agreement\", \"serial_number\": \"SN71\", \"additional_info\": {\"description\": \"A contract signed personally.\"}, \"order_info\": null}]}",
+        "status": "Created"
+      },
+      {
+        "creation": 1726414285.3262208,
+        "description": "Order is pending.",
+        "id": "66e6fdcd2db33881185dc1ed",
+        "order_info": "{\"_id\": \"66e6fdcd2db33881185dc1e3\", \"creation\": 1726414285.305219, \"updated\": 1726414285.305219, \"status\": null, \"status_trace\": [], \"client\": {\"name\": \"Client F\", \"email\": \"client6@example.com\"}, \"inventory_items\": [{\"_id\": \"66e6fdcd2db33881185dc1d5\", \"creation\": 1726414285.2872214, \"product_id\": 3, \"product_name\": \"ID Card\", \"serial_number\": \"SN32\", \"additional_info\": {\"description\": \"A personalized ID card with unique identification.\"}, \"order_info\": null}, {\"_id\": \"66e6fdcd2db33881185dc1dc\", \"creation\": 1726414285.2872214, \"product_id\": 7, \"product_name\": \"Personal On-place Agreement\", \"serial_number\": \"SN71\", \"additional_info\": {\"description\": \"A contract signed personally.\"}, \"order_info\": null}]}",
+        "status": "Pending"
+      },
+      {
+        "creation": 1726414285.328267,
+        "description": "Order has been delivered.",
+        "id": "66e6fdcd2db33881185dc1ee",
+        "order_info": "{\"_id\": \"66e6fdcd2db33881185dc1e3\", \"creation\": 1726414285.305219, \"updated\": 1726414285.305219, \"status\": null, \"status_trace\": [], \"client\": {\"name\": \"Client F\", \"email\": \"client6@example.com\"}, \"inventory_items\": [{\"_id\": \"66e6fdcd2db33881185dc1d5\", \"creation\": 1726414285.2872214, \"product_id\": 3, \"product_name\": \"ID Card\", \"serial_number\": \"SN32\", \"additional_info\": {\"description\": \"A personalized ID card with unique identification.\"}, \"order_info\": null}, {\"_id\": \"66e6fdcd2db33881185dc1dc\", \"creation\": 1726414285.2872214, \"product_id\": 7, \"product_name\": \"Personal On-place Agreement\", \"serial_number\": \"SN71\", \"additional_info\": {\"description\": \"A contract signed personally.\"}, \"order_info\": null}]}",
+        "status": "Delivered"
+      }
+    ],
+    "updated": 1726414285.3472202
   };
 
   override getData(): Object {
